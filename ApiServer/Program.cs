6cf2545533752e3349c54ea03b2a991faee72368
harshmangalam <harshdev8218@gmail.com>
2022-08-27 18:57:59 +0530

    added connection

--- ApiServer/Program.cs
+++ ApiServer/Program.cs
@@ -1,25 +1,39 @@
+using ApiServer.Models;
+// using ApiServer.Services;
+
+
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// add swagger for api documentation 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddControllers();
+builder.Services.Configure<MusicDatabaseSettings>(builder.Configuration.GetSection("MusicDatabase"));
+// builder.Services.AddSingleton<CategoryService>();
+
+
+
+// cors
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins, policy => { policy.WithOrigins("http://localhost:5173"); });
+});
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseCors(MyAllowSpecificOrigins);
+
+// configure swagger ui in development mode 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
